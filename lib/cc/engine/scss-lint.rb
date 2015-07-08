ab4cfd077921219272b4d596429892dd68594df8
mrb <michaelrbernstein@gmail.com>
2015-07-08 12:14:10 -0400

    Make engine spec compliant

--- lib/cc/engine/scss-lint.rb
+++ lib/cc/engine/scss-lint.rb
@@ -8,15 +8,17 @@ module CC
 
       def initialize(directory:, config_path:, io: STDOUT)
         self.directory = directory
-        self.engine_config = JSON.parse config_path
-        # self.engine_config = parse_config(config_path)
+        self.engine_config = parse_config(config_path)
         self.cli = ::SCSSLint::CLI.new
       end
 
       def run
         options = scss_lint_options
         Dir.chdir(directory) do
-          cli.send(:act_on_options, options)
+          begin
+            cli.send(:act_on_options, options)
+          rescue ::SCSSLint::Exceptions::NoFilesError
+          end
         end
       end
 
@@ -34,12 +36,11 @@ module CC
           files: [], # execute SCSSLint::CLI in the `directory` path
           excluded_files: excluded_files
         }
+
         if engine_config["config"]
           options[:config_file] = engine_config["config"]
         end
-        # if engine_config["config"] && File.exists?(engine_config["config"])
-        #   options[:config_file] = File.join(directory, engine_config["config"])
-        # end
+
         options
       end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,17 @@
 require 'minitest/autorun'
 require "minitest/reporters"
 require "mocha/mini_test"
+require "tempfile"
+
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
 
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), "../lib")))
 require "cc/engine/scss-lint"
+
+def with_config_file_contents(content)
+  file = Tempfile.new('config')
+  file.write(content)
+  file.close
+  yield(file.path)
+  file.unlink
+end
